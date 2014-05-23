1b50a49f1eff5f534e605ed4868fa8518b91632e
Rohan Mathur <rmathur101@gmail.com>
2014-05-22 22:32:25 -0500

    FacbookAuth and landing page

---
+++ PingItMobile/app/controllers/index_controller.rb
@@ -0,0 +1,12 @@
+class IndexController < UIViewController
+  def viewDidLoad
+    super
+    
+    self.view.backgroundColor = UIColor.redColor
+    # @window.backgroundColor = UIColor.blackColor
+
+  end
+
+
+
+end
