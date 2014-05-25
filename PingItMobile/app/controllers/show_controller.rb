6ec98e313e82d8de0c2912781de7a4174c16f85d
Rohan Mathur <rmathur101@gmail.com>
2014-05-25 18:41:32 -0500

    correct info on show page

--- PingItMobile/app/controllers/show_controller.rb
+++ PingItMobile/app/controllers/show_controller.rb
@@ -1,7 +1,9 @@
 class ShowController < UIViewController
   def viewDidLoad
     super
-    self.view.backgroundColor = UIColor.greenColor
+    canvasYellow = UIColor.colorWithRed(1.0, green: 0.89, blue: 0.51, alpha: 1.0)
+
+    self.view.backgroundColor = canvasYellow
     # self.
     p App::Persistence['show_info']
 
@@ -13,11 +15,41 @@ class ShowController < UIViewController
 
 
 
-    @event_label = UILabel.alloc.initWithFrame(CGRectZero)
-    @event_label.text = App::Persistence['show_info'][:title]
-    @event_label.sizeToFit
-    @event_label.center = CGPointMake(self.view.frame.size.width / 2, self.view.frame.size.height / 7)
-    self.view.addSubview(@event_label)
+
+    # (CGRectMake(10, 10, 100, 100)
+
+
+    @event_title = UILabel.alloc.initWithFrame(CGRectMake(20, 40, 300, 200))
+    @event_title.text = App::Persistence['show_info'][:title]
+    # @event_title.sizeToFit
+    # @event_title.center = CGPointMake(self.view.frame.size.width / 2, self.view.frame.size.height / 7)
+    self.view.addSubview(@event_title)
+
+    @event_description = UILabel.alloc.initWithFrame(CGRectMake(20, 70, 300, 200))
+    @event_description.text = App::Persistence['show_info'][:description]
+    # @event_description.sizeToFit
+    # @event_description.center = CGPointMake(self.view.frame.size.width / 2, self.view.frame.size.height / 6)
+    self.view.addSubview(@event_description)
+
+    @event_address = UILabel.alloc.initWithFrame(CGRectMake(20, 100, 300, 200))
+    @event_address.text = App::Persistence['show_info'][:address]
+    # @event_address.sizeToFit
+    # @event_address.center = CGPointMake(self.view.frame.size.width / 2, self.view.frame.size.height / 5)
+    self.view.addSubview(@event_address)
+
+    @event_status = UILabel.alloc.initWithFrame(CGRectMake(20, 130, 300, 200))
+    @event_status.text = App::Persistence['show_info'][:status]
+    # @event_status.sizeToFit
+    # @event_status.center = CGPointMake(self.view.frame.size.width / 2, self.view.frame.size.height / 4)
+    self.view.addSubview(@event_status)
+
+    @event_start = UILabel.alloc.initWithFrame(CGRectMake(20, 160, 300, 200))
+    @event_start.text = App::Persistence['show_info'][:start_time]
+    # @event_start.sizeToFit
+    # @event_start.center = CGPointMake(self.view.frame.size.width / 2, self.view.frame.size.height / 3)
+    self.view.addSubview(@event_start)
+
+
 
 
 
