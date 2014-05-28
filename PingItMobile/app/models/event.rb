0bb5b512b3878821cd0bb5cb7eb62be5353fcaa0
Rohan Mathur <rmathur101@gmail.com>
2014-05-28 12:16:02 -0500

    inspecting response within callback of set_radius request

--- PingItMobile/app/models/event.rb
+++ PingItMobile/app/models/event.rb
@@ -96,7 +96,7 @@ end
 # http://pingitt.herokuapp.com/phone/get_events
 # http://localhost:3000
   def self.get_events(user_and_location, &block)
-    BW::HTTP.get("http://pingitt.herokuapp.com/phone/get_events", payload: {data: user_and_location}) do |response|
+    BW::HTTP.get("http://localhost:3000/phone/get_events", payload: {data: user_and_location}) do |response|
       puts "RESPONSE FROM GET EVENTS REQUEST" 
       if response.ok?
         result_data = BW::JSON.parse(response.body.to_str)
@@ -109,7 +109,7 @@ end
 
 
   def self.create_event(new_event_data, &block)
-    BW::HTTP.get("http://pingitt.herokuapp.com/phone/create_event", payload: {data: new_event_data}) do |response|
+    BW::HTTP.get("http://localhost:3000/phone/create_event", payload: {data: new_event_data}) do |response|
       puts "RESPONSE FROM CREATE EVENT REQUEST"
       if response.ok?   
         result_data = BW::JSON.parse(response.body.to_str)
@@ -121,7 +121,7 @@ end
   end
 
   def self.send_rsvp_info(event_rsvp_info, &block)
-    BW::HTTP.get("http://pingitt.herokuapp.com/phone/register_rsvp_info", payload: {data: event_rsvp_info}) do |response|
+    BW::HTTP.get("http://localhost:3000/phone/register_rsvp_info", payload: {data: event_rsvp_info}) do |response|
       if response.ok?
         result_data = BW::JSON.parse(response.body.to_str)
         block.call(result_data)
--- PingItMobile/app/models/user.rb
+++ PingItMobile/app/models/user.rb
@@ -1,13 +1,14 @@
 class User
 
+  # http://pingitt.herokuapp.com/phone/checkforuser
   def self.verify_or_create_user(fb_info)
-    BW::HTTP.get("http://pingitt.herokuapp.com/phone/checkforuser", payload: {data: fb_info}) do |response|
+    BW::HTTP.get("http://localhost:3000/phone/checkforuser", payload: {data: fb_info}) do |response|
       # p response
     end
   end
 
   def self.set_radius(user_info) 
-    BW::HTTP.get("http://pingitt.herokuapp.com/phone/set_radius", payload: {data: user_info}) do |response|  
+    BW::HTTP.get("http://localhost:3000/phone/set_radius", payload: {data: user_info}) do |response|  
       if response.ok?
         result_data = BW::JSON.parse(response.body.to_str)
         block.call(result_data)
